d5323be711bd588a029aef811f97f54e761be315
MS-Cinti <szabadicintia@gmail.com>
2022-02-03 15:46:10 +0100

    added js function

---
+++ frontend/script.js
@@ -0,0 +1,20 @@
+function addTwoIntegers(firstInteger, secondInteger) {
+    //console.log(firstInteger + secondInteger);
+    //return firstInteger + secondInteger                         //leállítja a műveletet vagy visszaküld
+    //return "hello"
+    if(typeof firstInteger === "number" && typeof secondInteger === "number"){
+        return firstInteger + secondInteger
+    } else{
+        return "valamelyik argumentum nem szám, ezért a művelet nem elvégezhető"
+    }
+    
+
+}
+
+console.log (addTwoIntegers(12, 13) );
+console.log (addTwoIntegers(1920, 85) );
+console.log (addTwoIntegers(13, "alma") );
+console.log(0 === "0");
+console.log(0 === 0);
+console.log("0" === "0");
+console.log(addTwoIntegers (90, "90") );
